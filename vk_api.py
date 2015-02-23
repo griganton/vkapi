af08f6b1f1b56fc8ab23651f41d30eecb74a8f95
Anton Grigoryev <mail@griganton.ru>
2015-02-23 15:54:42 +0300

    Initial comment
    VK API initial commit

---
+++ vk_api.py
@@ -0,0 +1,28 @@
+__author__ = 'agrigoryev'
+__author__ = 'agrigoryev'
+
+import requests
+import json
+
+class VkApi:
+    def __init__(self):
+        self.token = 'e41798c2adfda856f830295ff6cd0a8e49c0aef43b706615a7555cba153d434c4f2db11c6b7941d482138'
+        self.version = '5.28'
+
+    def request(self, method, data={}):
+        data['access_token'] = self.token
+        data['v'] = self.version
+        r = requests.post("https://api.vk.com/method/"+method, data)
+        return json.loads(r.content.decode())
+
+    def send_photo_album(self, group_id, album_id, image, caption):
+        upload_server_info = self.request('photos.getUploadServer',{'album_id':album_id,
+                                                                     'group_id':group_id})['response']
+        send_file = requests.post(upload_server_info['upload_url'], files={"file1":("file1.jpg", image)})
+        photo_save_info = json.loads(send_file.content.decode())
+        photo_save_info['caption']= caption
+        photo_save_info['album_id']=photo_save_info['aid']
+        photo_save_info['group_id']=photo_save_info['gid']
+        r = self.request('photos.save',photo_save_info)
+        return r
+
